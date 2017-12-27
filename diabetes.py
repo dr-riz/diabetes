214815ff42bdb34041674ca8be9be0aefe22c420
dr-riz <vizvan@gmail.com>
2017-12-27 18:03:05 -0500

    creating normalize data set

--- diabetes.py
+++ diabetes.py
@@ -5,6 +5,8 @@ print("hw from python")
 import pandas
 from pandas.tools.plotting import scatter_matrix
 import matplotlib.pyplot as plt
+from sklearn import preprocessing as preproc
+import numpy as np
 
 datafile="./diabetes.data"
 headers=['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
@@ -31,16 +33,30 @@ print(dataset.groupby('class').size())
 print(" = 4. Data Visualization = ")
 # box and whisker plots
 print(" == 4.1 Univariate Plots: box and whisker plots. why? to determine outliers? = ")
-dataset.plot(kind='box', subplots=True, layout=(2,4), sharex=False, sharey=False)
-plt.show()
+#dataset.plot(kind='box', subplots=True, layout=(2,4), sharex=False, sharey=False)
+#plt.show()
 
 # histograms
 print(" == 4.1 Univariate Plots: histograms. why? to determine if the distribution is normal-like? == ")
-dataset.hist()
-plt.show()
+#dataset.hist()
+#plt.show()
 
 # scatter plot matrix
 print("== 4.2 Multivariate Plots: Multivariate Plots:scatter plot matrix. why? to spot structured relationships between input variables ==")
-scatter_matrix(dataset)
-plt.show()
+#scatter_matrix(dataset)
+#plt.show()
+
+array = np.array(dataset.values)
+X = array[:,0:8]
+Y = array[:,8]
+
+#X_norm = preproc.normalize(X, norm='l2', axis=0, copy=True, return_norm=True)
+
+scaler = preproc.MinMaxScaler()
+print(scaler.fit(X))
+preproc.MinMaxScaler(copy=True, feature_range=(0, 1))
+X_norm = scaler.transform(X)
+attr_norm = pandas.DataFrame(X_norm)
+print(attr_norm.describe())
+
 
